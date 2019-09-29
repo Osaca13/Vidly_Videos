12c40b6bde3de04c6b932c44fadbf9b4b74427e4
Osaca13 <odlyss@gmail.com>
2019-09-29 20:53:04 +0200

    Agregar archivos de proyecto.

---
+++ Vidly/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult TableShow() 
+        {
+            ViewBag.Message = "Show of all videos";
+
+            return View();
+          
+        }
+
+        public ActionResult Create() 
+        {
+            ViewBag.Message = "Create new link to a video";
+
+            return View();
+        
+        
+        }
+
+    }
+}
---
+++ Vidly/Models/VideoModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class VideoModel
+    {
+        [DisplayName("Id of Video")]
+        public int VideoId { get; set; }
+        [DisplayName("Título")]
+        public string Title { get; set; }
+        [DisplayName(@"Fecha de Creación")]
+        public DateTime CreatedDate { get; set; }
+
+        public Autor Author { get; set; }
+    }
+
+    public class Autor 
+    {
+        public int AutorId { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellidos { get; set; }
+
+    }
+
+
+}
+
